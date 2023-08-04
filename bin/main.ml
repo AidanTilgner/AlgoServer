8ef59bdfba8ac9c146e01f83fe43b0d6bb08631a
atilgner <aidantilgner02@gmail.com>
2023-08-03 23:15:50 -0700

    feat: added some more to the server

--- bin/main.ml
+++ bin/main.ml
@@ -1,4 +1,25 @@
+let count = ref 0
+
+let count_requests inner_handler request =
+  count := !count + 1;
+  inner_handler request
+
 let () =
   Dream.run ~port:3015
   @@ Dream.logger
-  @@ fun _ -> Dream.html "Good morning, world!"
+  @@ count_requests
+  @@ Dream.router [
+    Dream.get "/"
+    (fun _ ->
+      Dream.html "Good morning, world");
+    
+      Dream.get "/echo/:word"
+      (fun request ->
+        Dream.html (Dream.param request "word"));
+  
+      Dream.get "/app/**" (Dream.static "./public");
+  
+      Dream.get "/num-requests"
+        (fun _ ->
+          Dream.html (Printf.sprintf "Saw %i request(s)!" !count));
+  ]
